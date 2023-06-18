e2871d2eb3945ed4fde29e374e85745757dbdafd
Gbrittsz <hbankole06@gmail.com>
2023-06-18 07:26:37 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,20 +7,19 @@
  */
 int main(void)
 {
-	int i = '0';
+	int num;
 
-	while (i <= '9')
-	}
-                putchar(i);
-		if (i != '9')
-                {
-			putchar(',');
-			putchar(',');
-		}
-                ++!
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+		
+		putchar(',');
+		putchar(' ');
 	}
 
 	putchar('\n');
-	return (0);
 
+	return (0);
 }
